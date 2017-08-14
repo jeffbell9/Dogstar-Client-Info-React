ab2227d7c887d0430cf10006da8e8a2d0d8930be
Jeff Bell <jeferb9@gmail.com>
2017-08-14 06:43:32 -0700

    Fixed float wrap around issue

--- seed.js
+++ seed.js
@@ -8,7 +8,7 @@ Client.find({}, (err, clients) => {
     if(!clients.length) {
         Client.create({humanName: "Jeff",
                         dogName: "Luke",
-                        address: "3805 SW Admiral Way",
+                        address: "123 Somewhere Lane",
                         email: "human@gmail.com",
                         phone: "206-555-1234"    
         })
--- server.js
+++ server.js
@@ -14,22 +14,7 @@ let port = process.env.PORT || 8080;
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(parser.json());
 
-/*app.all('*',function(req,res,next){
-	if(req.headers['x-forwarded-proto']!=='https') {
-	  return res.redirect(`https://${req.hostname}${req.url}`);
-	} else {
-	  return next();
-	}
-});*/
-
 app.use('/api',router);
-
-/*https.createServer({
-	key: fs.readFileSync('key.pem'),
-	cert: fs.readFileSync('cert.pem')
-  }, app).listen(port, () => {
-	  console.log(`The server is running on port ${port} !`);
-});*/
 
 app.listen(port, () => {
 	console.log(`The server is running on port ${port} !`);
