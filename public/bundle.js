b8b6ef9e9589606953d3575beead84a102018a59
Jeff Bell <jeferb9@gmail.com>
2017-08-13 19:24:00 -0700

    Ran build

--- public/bundle.js
+++ public/bundle.js
@@ -23362,7 +23362,7 @@ function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { de
 
 var port = process.env.PORT || 8080;
 
-_reactDom2.default.render(_react2.default.createElement(_ClientInfo2.default, { url: "https://localhost:" + port + "/api/clients" }), document.getElementById('container'));
+_reactDom2.default.render(_react2.default.createElement(_ClientInfo2.default, { url: "http://localhost:" + port + "/api/clients" }), document.getElementById('container'));
 
 /* REACT HOT LOADER */ }).call(this); } finally { if (false) { (function () { var foundReactClasses = module.hot.data && module.hot.data.foundReactClasses || false; if (module.exports && module.makeHot) { var makeExportsHot = require("/Users/jeffbell/Desktop/Projects/dogstar-client-info-react/node_modules/react-hot-loader/makeExportsHot.js"); if (makeExportsHot(module, require("react"))) { foundReactClasses = true; } var shouldAcceptModule = true && foundReactClasses; if (shouldAcceptModule) { module.hot.accept(function (err) { if (err) { console.error("Cannot apply hot update to " + "app.js" + ": " + err.message); } }); } } module.hot.dispose(function (data) { data.makeHot = module.makeHot; data.foundReactClasses = foundReactClasses; }); })(); } }
 /* WEBPACK VAR INJECTION */}.call(exports, __webpack_require__(0)))
